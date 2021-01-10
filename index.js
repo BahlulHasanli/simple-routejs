f1c8c1c726075d26b77a6e3ce87a5f9de2a8a08a
BahlulHasanli <Behlulheseni@gmail.com>
2021-01-10 14:57:38 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use('/app', express.static(path.resolve(__dirname, 'src', 'app')));
+app.use("/app", express.static(path.resolve(__dirname, "src", "app")));
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('src', 'index.html'));
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve("src", "index.html"));
 });
 
-app.listen(3000, () => console.log('Server run up!'));
+app.listen(PORT, () => console.log("Server run up!"));
